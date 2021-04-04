bfed4db7cd51d087ba777277820138f1a4b7651c
devin <devin@nukon.com>
2021-04-04 19:26:03 +1000

    first init

---
+++ src/main/java/ordered/sftpetclinic/sftpetclinic/SftPetClinicApplication.java
@@ -0,0 +1,13 @@
+package ordered.sftpetclinic.sftpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ordered/sftpetclinic/sftpetclinic/SftPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ordered.sftpetclinic.sftpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
