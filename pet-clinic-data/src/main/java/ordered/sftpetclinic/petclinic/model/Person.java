039ff5b00365de4453c1eda7702378da1c99a68d
devin <devin@nukon.com>
2021-04-06 19:38:25 +1000

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/ordered/sftpetclinic/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ordered/sftpetclinic/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package ordered.sftpetclinic.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/ordered/sftpetclinic/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ordered/sftpetclinic/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package ordered.sftpetclinic.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private  String name;
 
     public String getName() {
